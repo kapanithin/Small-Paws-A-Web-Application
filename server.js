2525261424aad1dc65d2f875a2619eb5eb0a31df
kapanithin <56621818+kapanithin@users.noreply.github.com>
2022-07-05 23:05:00 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,34 @@
+const express =require("express");
+const mongoose =require("mongoose");
+const cors =require("cors");
+require("dotenv").config();
+
+//set up express
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT =process.env.PORT || 5000;
+
+app.listen(PORT,() => console.log(`THE SERVER HAS STARTED ON PORT : ${PORT}`));
+
+//set up mongoose
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+},(err) => {
+    if (err) throw err;
+    console.log("MongoDB connection established");
+});
+
+//set up route
+app.use("/users",require("./routes/userRouter"));
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static('frontend/build'));
+    const path = require('path');
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'))
+    })
+}
